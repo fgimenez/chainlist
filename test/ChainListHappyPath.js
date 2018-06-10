bfaa9ad982e9a4558424ba001a6273d142a50b99
Federico Gimenez <federico.gimenez@gmail.com>
2018-06-10 18:44:49 +0200

    fix test

--- test/ChainListHappyPath.js
+++ test/ChainListHappyPath.js
@@ -1,20 +1,18 @@
 var ChainList = artifacts.require("./ChainList.sol");
 
 contract('ChainList', ([creator, seller]) => {
-  var chainlistInstance;
-  var seller = seller;
-  var articleName = "article 1";
-  var articleDescription = "description for article 1";
-  var articlePrice = web3.toWei(10, "ether");
+  const articleName = "article 1";
+  const articleDescription = "description for article 1";
+  const articlePrice = web3.toWei(10, "ether");
 
   it("should be initialized with empty values", async () => {
     const instance = await ChainList.deployed();
     const data = await instance.getArticle();
 
-    assert.equal(data[0], creator, "initial seller must be creator");
-    assert.equal(data[1], "Default article", "initial article name must be default");
-    assert.equal(data[2], "Default description", "article description must be default");
-    assert.equal(data[3].toNumber(), 10000000000000000000, "article price be default");
+    assert.equal(data[0], 0x0, "initial seller must be creator");
+    assert.equal(data[1], "", "initial article name must be default");
+    assert.equal(data[2], "", "article description must be default");
+    assert.equal(data[3].toNumber(), 0, "article price be default");
   });
 
   it("should sell an article", async () => {
